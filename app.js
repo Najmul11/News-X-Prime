d3c784c40d95495da0092de6f64f876fcd7c7d40
NazPro <najmulbinnurul3@gmail.com>
2022-10-17 17:44:50 +0600

    news added

--- app.js
+++ app.js
@@ -13,3 +13,71 @@ const loadCatagories = () => {
       catagoryContainer.appendChild(div);
     });
   };
+
+  const loadNews = (id) => {
+    loadSpinner(true);
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displayNews(data.data))
+      .catch((err) => console.log(err));
+  };
+  const loadSpinner = (isLoading) => {
+    const spinner = document.getElementById("spinner");
+    if (isLoading) {
+      spinner.classList.remove("d-none");
+    } else {
+      spinner.classList.add("d-none");
+    }
+  };
+  const displayNews = (datas) => {
+    const newsFound = document.getElementById("message");
+    newsFound.classList.remove("d-none");
+    newsFound.innerHTML = `<h3 class=" text-center bg-white rounded py-3">${datas.length} recent news</h3>`;
+    const newsContainer = document.getElementById("news-container");
+    newsContainer.innerHTML = ``;
+    for (const data of datas) {
+      const { thumbnail_url, title, details, total_view, author, _id } = data;
+      const div = document.createElement("div");
+      div.classList.add("col-");
+      div.innerHTML = `
+              <div onclick="loadDetail('${_id}')" class="p-3 shadow-sm bg-white rounded d-flex gap-4 position-relative"  type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                  <div class="">
+                      <img src=${thumbnail_url} class= d">
+                  </div>
+                  <div>
+                      <h3>${title}</h3>
+                      <p class="mt-5">${details.slice(0, 400)}</p>
+                      <div class="d-flex  align-items-center justify-content-between  profile w-75">
+                          <div class="d-flex gap-3 align-items-center">
+                              <div>
+                                  <img src="${
+                                    author.img
+                                  }" alt="" class="rounded-circle">
+                              </div>
+                              <div class="mt-3">
+                                  <h5 class="">${
+                                    author.name ? author.name : "No author"
+                                  }</h5>
+                              </div>
+                          </div>
+                          <div>
+                              <p class="mt-4 fw-semibold"><i class="fa-regular fa-eye"></i> ${
+                                total_view ?? "No data"
+                              }</p>
+                          </div>
+                          <div>
+                              <i class="fa-solid fa-star text-dark"></i>
+                              <i class="fa-solid fa-star text-dark"></i>
+                              <i class="fa-solid fa-star text-dark"></i>
+                              <i class="fa-solid fa-star text-dark"></i>
+                              <i class="fa-regular fa-star text-dark"></i>
+                           </div>
+                      </div>
+                  </div>
+              </div>
+              `;
+      newsContainer.appendChild(div);
+    }
+    loadSpinner();
+  };
